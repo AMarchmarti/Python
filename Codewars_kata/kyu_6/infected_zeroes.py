ff6198f384496df8f6110db6e5187d170412f0a6
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2018-12-21 09:45:16 +0100

    Kata pass Test Cases but not optimize

--- Codewars_kata/kyu_6/infected_zeroes.py
+++ Codewars_kata/kyu_6/infected_zeroes.py
@@ -1,8 +1,13 @@
+'''In this kata, the number 0 is infected. You are given a list. Every turn, any item in the list that is adjacent to a 0 becomes infected and 
+transforms into a 0.  How many turns will it take for the whole list to become infected?
+All lists will contain at least one item, and at least one zero, and the only items will be 0s and 1s. 
+Lists may be very very long, so pure brute force approach will not work.'''
+
+
 def infected_zeroes(lst):
     turns = 0
     lsttemporal = []
-    for i in lst:
-        lsttemporal.append(i)
+    lsttemporal = lst[:]
     if lst.count(0) == len(lst):
         return turns
     while lst.count(0) != len(lst):
@@ -16,3 +21,16 @@ def infected_zeroes(lst):
             turns += 1
             lst = lsttemporal[:]
     return turns
+
+if __name__ == "__main__":
+    
+
+    # TEST CASES #
+
+
+    assert infected_zeroes([0]) == 0
+    assert infected_zeroes([0,1,1,0]) == 1
+    assert infected_zeroes([0,1,1,1,0]) == 2
+    assert infected_zeroes([1,1,0,1,1]) == 2
+    assert infected_zeroes([1,1,1,0]) == 3
+    assert infected_zeroes([0,1,1,1]) == 3
