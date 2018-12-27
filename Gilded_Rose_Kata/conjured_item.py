32416dae9d8902f9f9329a05be07c749cd8e23e3
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2018-12-27 11:56:54 +0100

    Creation of ConjuredItem

---
+++ Gilded_Rose_Kata/conjured_item.py
@@ -0,0 +1,20 @@
+from item import Item
+class ConjuredItem(Item):
+
+    def update_quality(self):
+
+        if self.sell_in > 0:
+            self.setQuality(-2)
+        else:
+            self.setQuality(-4)
+        self.setSell_in()
+
+if __name__ == "__main__":
+
+    # Correct Test Case
+    item = ConjuredItem("Elixir of mongoose", 5, 20)
+    print(item)
+
+    for dia in range(1, 10):
+        item.update_quality()
+        print(item)
