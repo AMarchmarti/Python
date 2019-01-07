146c053a1df9ec91abe438bdd25b6bafc6c4e5bc
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-01-07 11:43:59 +0100

    Exercise almost finished, add functions to convert de number of the month in str and print in console a string with the date. Add Tests Cases

--- POO/data.py
+++ POO/data.py
@@ -1,9 +1,15 @@
 class Data:
 
     def __init__(self, day, month, year):
-        self.day = "%02d" % int(1)
-        self.month = "%02d" % int(1)
-        self.year = 1900
+        try:
+            self.day = day
+            self.month = month
+            self.year = year   
+            assert self.day in range(1, 32) and self.month in range(1,13) and self.year in range(1900, 3001)
+        except AssertionError:
+            self.day = 1
+            self.month = 1
+            self.year = 1900
 
 
     def setDay(self, day):
@@ -11,7 +17,7 @@ class Data:
         try:
             self.day = day
         except AssertionError:
-            self.day = "%02d" % int(1)
+            self.day = 1
     def getDay(self, day):
         return self.day
 
@@ -20,7 +26,7 @@ class Data:
         try:
             self.month = month
         except AssertionError:
-            self.month = "%02d" % int(1)
+            self.month = 1
     def getMonth(self, month):
         return self.month
 
@@ -33,7 +39,7 @@ class Data:
     def getYear(self, year):
         return self.year
 
-
+    """
     def setDate(self, day, month, year):
         assert self.day in range(1, 32) and self.month in range(1,13) and self.year in range(1900, 3001)
         try:
@@ -41,31 +47,75 @@ class Data:
             self.month = month
             self.year = year   
         except AssertionError:
-            self.day = "%02d" % int(1)
-            self.month = "%02d" % int(1)
+            self.day = 1
+            self.month = 1
             self.year = 1900
-
+    """
     
     def addDays(self, numberdays):
+
         try:
-            for number in range(1, numberdays + 1):
-                self.day += number
+            while numberdays > 0:
+                self.day += 1
                 if self.day > 31:
                     self.day = 1
                     self.month += 1
                     if self.month > 12:
                         self.month = 1
                         self.year += 1
+                numberdays -= 1
             assert self.year in range(1900, 3001)
         except AssertionError:
-            self.day = "%02d" % int(1)
-            self.month = "%02d" % int(1)
+            self.day = 1
+            self.month = 1
             self.year = 1900
         except ValueError:
             print("%s not an integer" % numberdays)
 
     
     def __monthLetter(self):
-        months = ["January", "February", "March", "April", "May", "June", "July", ""]
+        months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+        keyMonth = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+        
+        numbermonth = dict(zip(keyMonth, months))
+        assert isinstance(numbermonth, dict)
+        assert numbermonth[6] == "June"
+
+        if self.month in numbermonth:
+            self.month = numbermonth[self.month]
+        return self
+        
+
+    def getDate(self):
+        return str(self.day) + "-" + self.month + "-" + str(self.year)
+        
+    
+    def printDate(self):
+        item = self.__monthLetter()
+        item = self.getDate()
+        print(item)
+
+if __name__ == "__main__":
+
+    #Tests Cases
+
+    data = Data(13, 4, 2030)
+    data.printDate()
+    print()
+    data2 = Data(14, 5, 2021)
+    data2.printDate()
+    print()
+    data_add = Data(21, 11, 2050)
+    data_add.addDays(1000)
+    data_add.printDate()
+
+
+
+
+
+                
+    
+
+
 
     
