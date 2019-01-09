c1b2a54ea4242a3d5b19d6f33f0155100169c76b
AMarchmarti <marchmarti.antoni@gmail.com>
2019-01-09 19:10:30 +0100

    Logic and Tests Cases

--- Gilded_Rose_Kata/gilded_rose.py
+++ Gilded_Rose_Kata/gilded_rose.py
@@ -1,18 +1,34 @@
 from regular_item import RegularItem
-from aged_brie import AgedBrie
+from agedbrie import AgedBrie
 from conjured_item import ConjuredItem
 from sulfuras import Sulfuras
 from backstage import Backstage
 
-class GildedRose:
+class GildedRose(object):
+
+    def __init__(self, items):
+        self.items = items
 
 
-
-
-
-
+    def update_quality(self):
+        for item in self.items:
+            item.update_quality()
 
 
 if __name__ == "__main__":
 
-    
+    #Tests Cases
+    Cheese = AgedBrie("Cheese", 3, 0)
+
+    MagicWater = ConjuredItem ("Water", 5, 10)
+
+    Entrance = Backstage("Metalica", 2, 23)
+
+    inventory = [Cheese, MagicWater, Entrance]
+
+    Tienda = GildedRose(inventory)
+    Tienda.update_quality()
+
+    assert Cheese.getQuality() == 1
+    assert Entrance.getQuality() == 26
+    assert MagicWater.getQuality() == 8
